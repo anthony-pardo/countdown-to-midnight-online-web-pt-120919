20abb39ae4a023625bf333b53bb0fd65e9a8ae94
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-12 06:27:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(integer)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  count = integer
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
